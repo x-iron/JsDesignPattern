1347fbe1626b7dcbc0f83a47d02965934842a92d
x-iron <alan.ye@XMN010.rcch.ringcentral.com>
2013-03-20 15:54:22 +0800

    bridge

--- bridge.js
+++ bridge.js
@@ -1,32 +1,52 @@
-function imp() {
-    this.show = function (str) {
-        alert(str);
-    };
+// Implementor 
+function sayHello() {
+	this.greet = function (str) {
+		alert('Hello, '+str);
+	};
 }
-  function imp2() {
-    this.show = function (str) {
-        alert(str+'!');
-    };
+// Implementor 
+function sayHi() {
+	this.greet = function (str) {
+		alert('Hi, '+str);
+	};
 }
-var i = new imp2();
 
-function abstract() {
-    var i;
-    this.setImp = function (imp) {
-        i = imp;
-    };
-    this.getImp = function () {
-        return i;
-    };
-}
-var abs = new abstract();
-abs.setImp(i);
-function refined() {
-    this.work = function (str) {
-        this.getImp().show(str);
-    };
-}
-refined.prototype = abs;
 
-var ref = new refined();
-ref.work('hello');
+// Abstraction
+function friend() {
+	var greeting;
+	this.setGreeting = function (greetingImp) {
+		greeting = greetingImp;
+	};
+	this.getGreeting = function () {
+		return greeting;
+	};
+}
+
+// Refined Abstraction
+function David() {
+	this.greet = function () {
+		this.getGreeting().greet('I am David.');
+	};
+}
+David.prototype = new friend();
+
+// Refined Abstraction
+function Lucy() {
+	this.greet = function () {
+		this.getGreeting().greet('I am Lucy.');
+	};
+}
+Lucy.prototype = new friend();
+
+var d = new David();
+d.setGreeting(new sayHi());
+d.greet();
+d.setGreeting(new sayHello());
+d.greet();
+
+var l = new Lucy();
+l.setGreeting(new sayHi());
+l.greet();
+l.setGreeting(new sayHello());
+l.greet();
