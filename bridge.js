9266b654d72b07a6f1bceae4f76fc8513c2de339
x-iron <alan.ye@XMN010.rcch.ringcentral.com>
2013-03-20 15:57:23 +0800

    bridge

--- bridge.js
+++ bridge.js
@@ -1,41 +1,41 @@
 // Implementor 
 function sayHello() {
-	this.greet = function (str) {
-		alert('Hello, '+str);
-	};
+    this.greet = function (str) {
+        alert('Hello, ' + str);
+    };
 }
 // Implementor 
 function sayHi() {
-	this.greet = function (str) {
-		alert('Hi, '+str);
-	};
+    this.greet = function (str) {
+        alert('Hi, ' + str);
+    };
 }
 
 
 // Abstraction
 function friend() {
-	var greeting;
-	this.setGreeting = function (greetingImp) {
-		greeting = greetingImp;
-	};
-	this.getGreeting = function () {
-		return greeting;
-	};
+    var greeting;
+    this.setGreeting = function (greetingImp) {
+        greeting = greetingImp;
+    };
+    this.getGreeting = function () {
+        return greeting;
+    };
 }
 
 // Refined Abstraction
 function David() {
-	this.greet = function () {
-		this.getGreeting().greet('I am David.');
-	};
+    this.greet = function () {
+        this.getGreeting().greet('I am David.');
+    };
 }
 David.prototype = new friend();
 
 // Refined Abstraction
 function Lucy() {
-	this.greet = function () {
-		this.getGreeting().greet('I am Lucy.');
-	};
+    this.greet = function () {
+        this.getGreeting().greet('I am Lucy.');
+    };
 }
 Lucy.prototype = new friend();
 
